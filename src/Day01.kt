d8625345562f0a4f66c4fc74cf0d9794efb17466
Destiro <connordebruyn234@gmail.com>
2021-12-07 15:54:41 +1300

    Completed day 1 task 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,13 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var decCount: Int = 0
+        var prevLine: String = input.get(0)
+        for (line in input){
+            if(line > prevLine){
+                decCount ++
+            }
+        }
+        return decCount
     }
 
     fun part2(input: List<String>): Int {
@@ -8,10 +15,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //val testInput = readInput("Day01_test")
+    //check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput("data/input")
     println(part1(input))
     println(part2(input))
 }
